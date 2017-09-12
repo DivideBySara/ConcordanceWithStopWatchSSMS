a529ac5ac6307737274285433db435b445d29612
dividebysara <s.jade@bellevuecollege.edu>
2017-09-12 10:43:57 -0700

    Add project files.

---
+++ Lab5/Concordance.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using static System.Console;
+using System.Collections.Generic;
+
+/*
+ * Sara Jade
+ * 9/11/17
+ * 
+ * TLG.Concordance has been modified to time how long it takes Analyzer.cs to run.
+ * Analyzer.cs code may be commented and uncommented between char[] and string
+ * 
+ */ 
+
+namespace TLG.Concordance
+{
+    class Concordance
+    {
+        static string inPath = string.Empty;
+        static string outPath = string.Empty;
+        static string excludedWordsPath = string.Empty;
+        static string inputText = string.Empty;
+        static string outputText = string.Empty;
+        internal static string[] excludedWords;
+
+        static void Main(string[] args)
+        {
+            const int NUM_RUNS = 10;
+            Analyzer anlz = new Analyzer();
+            Stopwatch sw = new Stopwatch();
+            double timeElapsed = 0.0;
+            List<TimeSpan> timespans = new List<TimeSpan>();
+            double totalRunTime = 0.0;
+
+            // Set up paths from args
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid number of path specifications");
+                return;
+            }
+            GetPaths(args);
+            // Get the input data
+            ReadInputs();
+            // Identify paragraphs and sentences
+            // Identify words and their location
+
+            // Start and stop stopwatch object
+            for (int i = 0; i < NUM_RUNS; ++i)
+            {
+                sw.Start();
+                anlz.Analyze(inputText);
+                sw.Stop();
+
+                // Display stopwatch time
+                timespans.Add(new TimeSpan());
+                timespans[i] = sw.Elapsed;
+                timeElapsed = timespans[i].TotalMilliseconds;
+                WriteLine($"Runtime is {timeElapsed}ms");
+            }
+
+            foreach (TimeSpan timespan in timespans)
+            {
+                totalRunTime += timespan.TotalMilliseconds;
+            }
+
+            WriteLine($"\nAverage run time = {totalRunTime / NUM_RUNS}ms");
+
+            // SQL connection strings are very unforgiving as to content and punctuation;
+            // A good way to obtain one is to set a data connection in the Visual Studio
+            // Server Explorer.  The connection string may be copied from the 
+            // connection properties.
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=Concordance;Integrated Security=True";
+            // A SQL connection is a scarce resource and must be released when not needed.
+            // The using block below will cause the connection to be cleaned up
+            // when the scope is exited.  The behavior is similar to that of a 
+            // finally block in structured exception handling.
+            // A using block should be written whenver possible to ensure you do not 
+            // forget to release a connection.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create an INSERT statement using parameters
+                // Always use parameters as they are not parsed and thus will not be a SQL injection path
+                string insertStmt = "INSERT INTO [AChristmasCarol].[WordRefs] (Word, ParagraphIndex, SentenceIndex, WordPositionIndex) VALUES(@word, @paragraph, @sentence, @wordPosition);";
+                // Statements to clear previous run from tables
+                string truncateStmtRefs = "TRUNCATE TABLE [AChristmasCarol].[WordRefs];";
+                string truncateStmtWords = "TRUNCATE TABLE [AChristmasCarol].[UniqueWords];";
+                // Statement to build unique word table
+                string uniqueWordStmt = "INSERT INTO [AChristmasCarol].[UniqueWords] SELECT DISTINCT [Word] FROM [AChristmasCarol].[WordRefs];";
+                // Set up a command object
+                using (SqlCommand cmd = new SqlCommand(truncateStmtRefs, connection))
+                {
+                    connection.Open();
+
+                    // Execute the truncations
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = truncateStmtWords;
+                    cmd.ExecuteNonQuery();
+                    
+                    // Set up the Wordref insertion
+                    cmd.CommandText = insertStmt;
+
+                    // Define parameters
+                    cmd.Parameters.Add("@word", SqlDbType.NVarChar);
+                    cmd.Parameters.Add("@paragraph", SqlDbType.Int);
+                    cmd.Parameters.Add("@sentence", SqlDbType.Int);
+                    cmd.Parameters.Add("@wordPosition", SqlDbType.Int);
+
+                    // Loop through the Wordref list
+                    foreach (Wordref wdref in anlz.wordRefs)
+                    {
+                        //cmd.Parameters["@word"].Value = new string(wdref.word);
+                        cmd.Parameters["@word"].Value = wdref.word;
+                        cmd.Parameters["@paragraph"].Value = wdref.paragraphIndex;
+                        cmd.Parameters["@sentence"].Value = wdref.sentenceIndex;
+                        cmd.Parameters["@wordPosition"].Value = wdref.wordPositionIndex;
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Build the UniqueWords table
+                    cmd.Parameters.Clear();
+                    cmd.CommandText = uniqueWordStmt;
+                    cmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+            ReadKey();
+        } // End Main()
+
+        static void ReadInputs()
+        {
+            try
+            {
+                inputText = File.ReadAllText(inPath);
+                excludedWords = File.ReadAllLines(excludedWordsPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading file " + e.Message);
+            }
+        }
+
+        static void GetPaths(string[] args)
+        {
+            // Get the path to the input text file
+            try
+            {
+                inPath = Path.GetFullPath(args[0]);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Invalid text input path {args[0]}");
+            }
+
+            // Get the path for the output file
+            try
+            {
+                outPath = Path.GetFullPath(args[1]);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Invalid text output path {args[1]}");
+            }
+
+            // Get the path to the excluded words file
+            try
+            {
+                excludedWordsPath = Path.GetFullPath(args[2]);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Invalid excluded words input path {args[1]}");
+            }
+        }
+
+    }
+}
